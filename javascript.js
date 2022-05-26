c4358284f5d3f5075555e3aaf35c7c3df52c1987
Yersoltan <natlosrey@gmail.com>
2022-05-26 14:54:28 +0600

    Add different paintbrush modes: white-color, rgb, eraser. Add clear button

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
-let gridBtn = document.querySelector('#grid-scale-button');
-let gridSize = document.querySelector('#grid-scale');
 let container = document.querySelector('.grid-container');
+let gridSize = document.querySelector('#grid-scale');
+let gridBtn = document.querySelector('#grid-scale-button');
+let colorBtn = document.querySelector('#color-button');
+let rgbBtn = document.querySelector('#rgb-button');
+let eraserBtn = document.querySelector('#eraser-button');
+let clearBtn = document.querySelector('#clear-button')
+let h1 = document.querySelector('h1');
 let paintbrushMode = 'color';
 
 function buildGrid() {
@@ -30,7 +35,34 @@ function clearGrid(container)
 container.addEventListener('mouseover', function(e) {
     let target = e.target.closest('.cell');
     if (!target) return;
-    target.style.backgroundColor = 'pink';
+    if(paintbrushMode == 'color') {
+        target.style.backgroundColor = 'white';
+    }
+    if(paintbrushMode == 'eraser') {
+        target.style.backgroundColor = 'black';
+    }
+    if(paintbrushMode == 'rgb') {
+        let r = Math.floor(Math.random() * 256)
+        let g = Math.floor(Math.random() * 256)
+        let b = Math.floor(Math.random() * 256)
+        target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    }
 });
 
+
+colorBtn.addEventListener('click', () => {
+    paintbrushMode = 'color';
+});
+rgbBtn.addEventListener('click', () => {
+    paintbrushMode = 'rgb';
+});
+rgbBtn.addEventListener('click', () => {
+    rgbBtn.classList.toggle('rainbowRGB');
+});
+eraserBtn.addEventListener('click', () => {
+    paintbrushMode = 'eraser';
+});
+clearBtn.addEventListener('click', () => {
+    clearGrid(container);
+});
 gridBtn.addEventListener('click', buildGrid);
